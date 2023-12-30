e56f523a372736b1f0c3d125afd0615dc0772fed
Sagar Patil <sagarreddypatil@gmail.com>
2023-12-29 21:10:41 -0500

    move monitor into courselib

--- courselib/__init__.py
+++ courselib/__init__.py
@@ -1,3 +1,4 @@
 from .types import Semester, Term, Section, Course, Seats, SectionSeats
 from .course import update_course_sections, section_seats
 from .seats_diff import diff_section_seats_pretty, diff_seats_pretty
+from .monitor import CourseMonitor
